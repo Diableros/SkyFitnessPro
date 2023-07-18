71bf0660bb49abb30fcea989a39caf7198f01dca
Ivan Shchipin <ivan.shchipin@gmail.com>
2023-07-18 09:56:03 +0300

    fix: изменение настроек пакета

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
 
@@ -6,7 +7,7 @@ import svgr from 'vite-plugin-svgr'
 export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
-    alias: { '@': '/src' },
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
   server: {
     port: 3000,
