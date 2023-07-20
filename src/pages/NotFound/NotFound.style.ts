93abaa917cb867e834f0dcf5f326ff3f347051e3
Ivan Shchipin <ivan.shchipin@gmail.com>
2023-07-20 22:56:00 +0300

    fix: поправлены стили страниц

--- src/pages/NotFound/NotFound.style.ts
+++ src/pages/NotFound/NotFound.style.ts
@@ -5,7 +5,7 @@ export const PageWrapper = styled.div`
   align-items: center;
   justify-content: center;
 
-  height: 100%;
+  min-height: 100vh;
   width: 100%;
 
   background-color: ${({ theme }) => theme.colors.purpleNormal};
