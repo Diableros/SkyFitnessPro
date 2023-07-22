458f923aeeb10e9ae035ce5d4ead43d74b116c69
Zhanna Kulaginae <kuljeanne@gmail.com>
2023-07-22 22:03:26 +0300

    feat: добавила страницу профиля

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,5 +1,53 @@
+import { ButtonSize, ButtonTheme } from '@/components/UiButton/enums'
+import UiButton from '@/components/UiButton/UiButton'
+import UiCourseCard from '@/components/UiCourseCard'
+import { PageType } from '@/components/UiCourseCard/enums'
+import { mockData } from '../Home/mockData'
+
+import * as S from './Profile.style'
+
+import { user } from './mockUserData'
+
+const { login, password, coursesId } = user
+const userCourses = mockData.filter((course) => coursesId.includes(course._id))
+
 const Profile = () => {
-  return <div>Text from Profile component</div>
+  return (
+    <S.PageWrapper>
+      <S.ProfileDataBlock>
+        <S.ProfileHeader>Мой профиль</S.ProfileHeader>
+        <S.ProfileData>
+          <S.ProfileDataItem>Логин: {login}</S.ProfileDataItem>
+          <S.ProfileDataItem>Пароль: {password}</S.ProfileDataItem>
+        </S.ProfileData>
+        <S.ProfileChangeBtns>
+          <UiButton
+            title="Редактировать логин"
+            buttonTheme={ButtonTheme.PurpleBright}
+            fontSize="s"
+            size={ButtonSize.L}
+          ></UiButton>
+          <UiButton
+            title="Редактировать пароль"
+            buttonTheme={ButtonTheme.PurpleBright}
+            fontSize="s"
+            size={ButtonSize.L}
+          ></UiButton>
+        </S.ProfileChangeBtns>
+      </S.ProfileDataBlock>
+        <S.ProfileHeader>Мои курсы</S.ProfileHeader>
+        <S.ProfileCourses>
+          {userCourses.length > 0 &&
+            userCourses.map((course) => (
+              <UiCourseCard
+                key={course._id}
+                course={course}
+                $pageType={PageType.Profile}
+              />
+            ))}
+        </S.ProfileCourses>
+    </S.PageWrapper>
+  )
 }
 
 export default Profile
