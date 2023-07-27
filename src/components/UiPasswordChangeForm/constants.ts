c309e9a3e7dba003cb1a6ea56901700f8f0797be
yazakhariya <17070501.iana@gmail.com>
2023-07-27 19:01:00 +0300

    feature: добавлен файл constants

--- src/components/UiPasswordChangeForm/constants.ts
+++ src/components/UiPasswordChangeForm/constants.ts
@@ -1,9 +1,9 @@
-const password_regex = '^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{8,}$'
+// const password_regex = '^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{8,}$'
 
-const passwordRequirements = {
-    required: 'Обязательное поле для ввода',
-    minLength: {
-        value: '8',
-        message: ,
-    },
-}
+// const passwordRequirements = {
+//     required: 'Обязательное поле для ввода',
+//     minLength: {
+//         value: '8',
+//         message: ,
+//     },
+// }
