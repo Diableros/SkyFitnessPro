5cb612dd715c4ec756d53ddb653adc773c95ad21
Ivan Shchipin <ivan.shchipin@gmail.com>
2023-07-27 21:14:24 +0300

    fix: restore workout.tsx

--- src/pages/Workout/Workout.tsx
+++ src/pages/Workout/Workout.tsx
@@ -1,7 +1,48 @@
-import UiProgressForm from './components/ProgressForm'
+import CourseVideo from './components/CourseVideo'
+import UserProgress from './components/UserProgress'
+import UiButton from '@/components/UiButton'
+import { ButtonSize, ButtonTheme } from '@/components/UiButton/enums'
+import { mockData } from '../Home/mockData'
+
+import * as S from './Workout.styles'
+
+import { userProgress, workoutData } from './mockData'
+const { courseId, exercises, name, video } = workoutData
+
+const course = mockData.find((course) => course._id === courseId)
 
 const Workout = () => {
-  return <UiProgressForm />
+  const handleButtonClick = () => {
+    console.log('click to wride down the progress')
+  }
+
+  return (
+    <S.PageWrapper>
+      <S.PageHeader>{course?.nameRU}</S.PageHeader>
+      <CourseVideo title={name} src={video} />
+
+      <S.ProgressBlock>
+        <S.Workouts>
+          <S.WorkoutsHeader>Упражнения</S.WorkoutsHeader>
+          <S.WorkoutList>
+            {exercises.map(({ name }, i) => (
+              <li key={i}>{name}</li>
+            ))}
+          </S.WorkoutList>
+
+          <UiButton
+            onClick={handleButtonClick}
+            title="Заполнить свой прогресс"
+            buttonTheme={ButtonTheme.PurpleBright}
+            fontSize="s"
+            size={ButtonSize.L}
+          />
+        </S.Workouts>
+
+        <UserProgress exercises={userProgress} />
+      </S.ProgressBlock>
+    </S.PageWrapper>
+  )
 }
 
 export default Workout
