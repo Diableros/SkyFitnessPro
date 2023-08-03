85f5d9521cfa414d7030260ed181b495ad3f1924
Ivan Shchipin <ivan.shchipin@gmail.com>
2023-08-03 13:43:48 +0300

    fix: вот что я имел ввиду

--- src/components/UiModal/UiModal.tsx
+++ src/components/UiModal/UiModal.tsx
@@ -4,31 +4,22 @@ import UiImage from '../UiImage'
 
 import * as S from './UiModal.style'
 
-type ChildProps = {
-  onClose: () => void
-}
-
 type PropsType = {
-  children: React.ReactElement<ChildProps>
-  modalClose:()=> void
+  children: React.ReactNode
+  modalClose: () => void
 }
 
-const UiModal = ({ children,modalClose}: PropsType) => {
- 
-
-  const Content = children.type
-
-
+const UiModal = ({ children, modalClose }: PropsType) => {
   return (
     <S.ModalScreen>
       <S.ModalContentBox>
         <S.ImageWrapper onClick={modalClose}>
           <UiImage name="cross" width="20px" height="20px" />
         </S.ImageWrapper>
-        <Content {...children.props} onClose={modalClose} />
+        {children}
       </S.ModalContentBox>
     </S.ModalScreen>
-  ) 
+  )
 }
 
 export default UiModal
